f57566ac963dc479d9e41c403644b7cb022f0a5c
Jeremy Hotz <JeremyHotz@Jeremys-iMac-2.local>
2021-08-14 12:35:16 -0600

    update about me

--- src/components/GoogleMapApi/GoogleMapApi.jsx
+++ src/components/GoogleMapApi/GoogleMapApi.jsx
@@ -1,36 +1,36 @@
-import React, { Component } from 'react';
-export default function GoogleMapApi() {
+// import React, { Component } from 'react';
+// export default function GoogleMapApi() {
 
-    return (
-        <script>
+//     return (
+//         <script>
 
-            {const selectEl = document.getElementById("id_rating");
-            M.FormSelect.init(selectEl);
+//             {const selectEl = document.getElementById("id_rating");
+//             M.FormSelect.init(selectEl);
 
-            const address = {location.address};
-            const geoUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key={{ api_key }}`;
-            let geoResults, validAddress;
-                fetch(geoUrl).then(res => res.json())
-                    .then(data => {
-                validAddress = data.status === "OK";
-            if (validAddress) geoResults = data.results[0];
-            if (validAddress) {
-                let location = geoResults.geometry.location;
-            let map;
-            map = new google.maps.Map(
-            document.getElementById("map"),
-            {
-                center: location,
-            zoom: 12,
-            }
-            );
-            const marker = new google.maps.Marker({
-                position: location,
-            map: map,
-        });
-      }
-    })
-        </script>
-    )
-    s
-}
+//             const address = {location.address};
+//             const geoUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key={{ api_key }}`;
+//             let geoResults, validAddress;
+//                 fetch(geoUrl).then(res => res.json())
+//                     .then(data => {
+//                 validAddress = data.status === "OK";
+//             if (validAddress) geoResults = data.results[0];
+//             if (validAddress) {
+//                 let location = geoResults.geometry.location;
+//             let map;
+//             map = new google.maps.Map(
+//             document.getElementById("map"),
+//             {
+//                 center: location,
+//             zoom: 12,
+//             }
+//             );
+//             const marker = new google.maps.Marker({
+//                 position: location,
+//             map: map,
+//         });
+//       }
+//     })
+//         </script>
+//     )
+//     s
+// }
--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,5 +1,10 @@
 export default function AboutPage() {
     return (
-        <h1>AboutPage</h1>
+        <>
+        <h1>About</h1>
+        <div className="form-container">
+        <p className="form">Have you ever wanted to go kayaking, boating, tubing or just to a gorgeous place and had no idea where to go?<br></br><br></br>  <u>Well you've come to the right place.</u><br></br><br></br>  The goal of this page is for Outdoors enthusiasts, of any skill level, to share their "Spot" and to see other cool "Spots" to visit.</p>
+        </div>
+        </>
     )
 }
